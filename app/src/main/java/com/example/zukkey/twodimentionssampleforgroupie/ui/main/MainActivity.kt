56f01ecd949b0f1b8af410f8730009f4386e4c46
zukkey <y_vkwi_27_hunhun@yahoo.co.jp>
2019-02-02 22:53:01 +0900

    wip

--- app/src/main/java/com/example/zukkey/twodimentionssampleforgroupie/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/zukkey/twodimentionssampleforgroupie/ui/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.zukkey.twodimentionssampleforgroupie.MainApp
 import com.example.zukkey.twodimentionssampleforgroupie.R
+import com.example.zukkey.twodimentionssampleforgroupie.databinding.ActivityMainBinding
 import com.example.zukkey.twodimentionssampleforgroupie.di.module.MainModule
 import javax.inject.Inject
 
@@ -11,10 +12,15 @@ class MainActivity : AppCompatActivity() {
 
     @Inject lateinit var viewModel: MainViewModel
 
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         val component = (application as MainApp).component
         component.plus(MainModule(this)).inject(this)
+        binding.vm = viewModel
     }
 }
