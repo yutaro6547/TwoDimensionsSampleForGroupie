12b0ccb4120fb50735dcb79cfd5edd3ad69e08fc
zukkey <y_vkwi_27_hunhun@yahoo.co.jp>
2019-02-02 21:04:35 +0900

    refactor

--- app/src/main/java/com/example/zukkey/twodimentionssampleforgroupie/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/zukkey/twodimentionssampleforgroupie/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.zukkey.twodimentionssampleforgroupie
+package com.example.zukkey.twodimentionssampleforgroupie.ui.main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.zukkey.twodimentionssampleforgroupie.R
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/com/example/zukkey/twodimentionssampleforgroupie/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/zukkey/twodimentionssampleforgroupie/ui/main/MainViewModel.kt
@@ -1,6 +1,7 @@
-package com.example.zukkey.twodimentionssampleforgroupie
+package com.example.zukkey.twodimentionssampleforgroupie.ui.main
 
 import android.arch.lifecycle.AndroidViewModel
+import com.example.zukkey.twodimentionssampleforgroupie.MainApp
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
