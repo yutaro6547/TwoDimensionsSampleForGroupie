375d1c0a2231fb8d68f55cb46d0bfdc8226889e3
zukkey <y_vkwi_27_hunhun@yahoo.co.jp>
2019-02-02 21:37:04 +0900

    add rx

--- app/src/main/java/com/example/zukkey/twodimentionssampleforgroupie/api/GitHubClient.kt
+++ app/src/main/java/com/example/zukkey/twodimentionssampleforgroupie/api/GitHubClient.kt
@@ -1,6 +1,6 @@
 package com.example.zukkey.twodimentionssampleforgroupie.api
 
-import retrofit2.Call
+import io.reactivex.Observable
 import retrofit2.http.GET
 import javax.inject.Inject
 
@@ -8,12 +8,12 @@ class GitHubClient @Inject constructor(
     private val service: GitHubService
 ) {
 
-    fun getTargetUser(): Call<List<GitHubUser>> {
+    fun getTargetUser(): Observable<List<GitHubUser>> {
         return service.getUsers()
     }
 
     interface GitHubService {
         @GET("users")
-        fun getUsers(): Call<List<GitHubUser>>
+        fun getUsers(): Observable<List<GitHubUser>>
     }
 }
